c31ad388102acbe7aad2563e279116eca6030f74
Stenli777 <alekseysten777@yandex.ru>
2022-08-14 18:10:14 +0300

    task 5 done

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Stenli777/php-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Stenli777/php-project-lvl1/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/b8cc07e1a7df1f0e1cae/maintainability)](https://codeclimate.com/github/Stenli777/php-project-lvl1/maintainability)
+
+https://asciinema.org/a/VzenyGkutuscIhbzc87jIRknn
--- src/Even.php
+++ src/Even.php
@@ -11,21 +11,21 @@ function game()
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    line('');
-    $question1 = prompt('Question: 15');
-    if ($question1 == 'no') {
-    $result1 = 'Correct!';} else {
-    return line("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, %s!", $name);
+    $maxNum = 1;
+    $minNum = 100;
+    for ($i=0;$i<3;$i++) {
+    $random = rand($minNum,$maxNum);
+    $answer = prompt("Question: {$random}");
+    line("Your answer: {$answer}");
+    $correctAnswer = $random % 2 === 0 ? 'yes' : 'no';
+    if ($answer == $correctAnswer) {
+        line('Correct!');} 
+        else {
+        line ("'{$answer}' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+        line("Let's try again, %s!", $name);
+        return;
     }
-    $question2 = prompt('Question: 6');
-    if ($question2 == 'yes') {
-    $result2 = 'Correct!';} else {
-    return line("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, %s!", $name);
-    }
-    $question3 = prompt('Question: 7');
-    if ($question3 == 'no') {
-    $result3 = 'Correct!';} else {
-    return line("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, %s!", $name);
-    }
-    return line("Congratulations, %s!", $name);
+
+}
+    line("Congratulations, %s!", $name);
 }
